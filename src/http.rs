65535ac43774a10ce4d04fe79130edb588851e5f
Dane Murphy <danem7@gmail.com>
2022-07-12 19:39:24 -0700

    check for path in hashmap

--- src/http.rs
+++ src/http.rs
@@ -46,27 +46,29 @@ impl HTTPRequest {
         let protocol = "HTTP/1.1".to_string();
         let buffer_str = String::from_utf8(buffer.to_vec()).unwrap();
         let p: Vec<&str> = buffer_str.split(' ').collect();
-        let path = p.get(1).unwrap().to_string();
-        let mut headers = HeaderMap::new();
+        if let Some(path_str) = p.get(1) {
+            let path = path_str.to_string();
+            let mut headers = HeaderMap::new();
 
-        for line in buffer_str.split("\r\n").into_iter().skip(1) {
-            debug!("line: {}", line);
+            for line in buffer_str.split("\r\n").into_iter().skip(1) {
+                debug!("line: {}", line);
 
-            if let Some(splitter) = line.find(": ") {
-                let header_key = String::from(&line[..splitter]);
-                let header_value = String::from(&line[splitter + 2..]);
+                if let Some(splitter) = line.find(": ") {
+                    let header_key = String::from(&line[..splitter]);
+                    let header_value = String::from(&line[splitter + 2..]);
 
-                headers.insert(
-                    HeaderName::from_str(header_key.as_str()).unwrap(),
-                    header_value.as_str().parse().unwrap(),
-                );
+                    headers.insert(
+                        HeaderName::from_str(header_key.as_str()).unwrap(),
+                        header_value.as_str().parse().unwrap(),
+                    );
+                }
             }
-        }
 
-        self.method = method;
-        self.path = path;
-        self.protocol = protocol;
-        self.headers = headers;
-        self.body = body;
+            self.method = method;
+            self.path = path;
+            self.protocol = protocol;
+            self.headers = headers;
+            self.body = body;
+        }
     }
 }
